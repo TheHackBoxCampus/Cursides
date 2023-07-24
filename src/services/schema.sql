ed94d564181dd83e2620da28fa86b22965ef37fe
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-23 21:58:21 -0500

    feat: :sparkles: 🔄 Estructura ``SQL``
    Se actualizo la estructura de las consultas sql, para la creacion de la msima.
    
    footer: comandos ddl, dql, dml

--- src/services/schema.sql
+++ src/services/schema.sql
@@ -1,110 +1,424 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-07-2023 a las 04:54:35
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
-USE cursideslearn;
-DROP DATABASE cursideslearn;
-
-CREATE TABLE Usuario (
-    id_usuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_usuario VARCHAR(50) NOT NULL,
-    email_usuario VARCHAR(50) UNIQUE NOT NULL,
-    contraseña_usuario VARCHAR(50) NOT NULL,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-); 
-
-CREATE TABLE Inscripcion (
-    id_inscripcion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT, 
-    id_curso INT,
-    inscrito_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Curso (
-    id_curso INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_curso VARCHAR(50) UNIQUE NOT NULL,
-    descripcion TEXT NOT NULL,
-    n_horas INT NOT NULL,
-    id_categoria INT,
-    id_docente INT,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Categoria (
-    id_categoria INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_categoria VARCHAR(50) NOT NULL
-); 
-
-CREATE TABLE Docente (
-    id_docente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_docente VARCHAR(50) NOT NULL,
-    id_especializacion INT
-);
-
-CREATE TABLE Especializacion (
-    id_especializacion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_especializacion VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Estado_leccion (
-    id_estado INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_estado VARCHAR(50) NOT NULL
-); 
-
-CREATE TABLE Leccion (
-    id_leccion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_leccion VARCHAR(50) NOT NULL,
-    descripcion TEXT NOT NULL,
-    duracion VARCHAR(50) NOT NULL,
-    id_estado_leccion INT,
-    id_curso INT
-);
-
-CREATE TABLE Capitulo (
-    id_capitulo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nombre_capitulo VARCHAR(50) NOT NULL,
-    duracion_capitulo VARCHAR(50) NOT NULL,
-    id_leccion INT
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-ALTER TABLE Inscripcion 
-ADD CONSTRAINT fk_usuario_inscripcion
-FOREIGN KEY(id_usuario)
-REFERENCES Usuario(id_usuario);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-ALTER TABLE Inscripcion 
-ADD CONSTRAINT fk_curso_inscripcion
-FOREIGN KEY (id_curso)
-REFERENCES Curso(id_curso);
+--
+-- Base de datos: `cursideslearn`
+--
 
-ALTER TABLE Curso 
-ADD CONSTRAINT fk_docente_curso
-FOREIGN KEY (id_docente) 
-REFERENCES Docente(id_docente); 
+-- --------------------------------------------------------
 
-ALTER TABLE Curso 
-ADD CONSTRAINT fk_categoria_curso
-FOREIGN KEY (id_categoria) 
-REFERENCES Categoria(id_categoria); 
+--
+-- Estructura de tabla para la tabla `capitulo`
+--
 
-ALTER TABLE Docente 
-ADD CONSTRAINT fk_especializacion_docente
-FOREIGN KEY (id_especializacion)
-REFERENCES Especializacion(id_especializacion);
+CREATE TABLE `capitulo` (
+  `id_capitulo` int(11) NOT NULL,
+  `nombre_capitulo` varchar(50) NOT NULL,
+  `duracion_capitulo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE Leccion 
-ADD CONSTRAINT fk_curso_leccion
-FOREIGN KEY (id_curso)
-REFERENCES Curso(id_curso);
+--
+-- Volcado de datos para la tabla `capitulo`
+--
 
+INSERT INTO `capitulo` (`id_capitulo`, `nombre_capitulo`, `duracion_capitulo`) VALUES
+(1, 'Formulario', 1),
+(2, 'Metadatos', 1);
 
-ALTER TABLE Leccion
-ADD CONSTRAINT fk_estado_leccion
-FOREIGN KEY (id_estado_leccion)
-REFERENCES Estado_leccion(id_estado);
+-- --------------------------------------------------------
 
-ALTER TABLE Capitulo
-ADD CONSTRAINT fk_leccion_capitulo
-FOREIGN KEY (id_leccion)
-REFERENCES Leccion(id_leccion);
+--
+-- Estructura de tabla para la tabla `categoria`
+--
 
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO Usuario (nombre_usuario, email_usuario, contraseña_usuario) VALUES ("miller", "kaled@narino.com", "miller2005");
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`) VALUES
+(1, 'Diseño de Bases de datos'),
+(3, 'UX|UI'),
+(4, 'infraestructura'),
+(5, 'Ciberseguridad');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `nombre_curso` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `n_horas` int(11) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_docente` int(11) DEFAULT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `nombre_curso`, `descripcion`, `n_horas`, `id_categoria`, `id_docente`, `creado_en`) VALUES
+(1, 'HTML', 'Aprende a crear paginas web, Estudia HTML!', 10, 3, 2, '2023-07-21 00:15:33');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+CREATE TABLE `docente` (
+  `id_docente` int(11) NOT NULL,
+  `nombre_docente` varchar(50) NOT NULL,
+  `id_especializacion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `docente`
+--
+
+INSERT INTO `docente` (`id_docente`, `nombre_docente`, `id_especializacion`) VALUES
+(1, 'Jolhver', 2),
+(2, 'Miguel', 1),
+(3, 'Bermen', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especializacion`
+--
+
+CREATE TABLE `especializacion` (
+  `id_especializacion` int(11) NOT NULL,
+  `nombre_especializacion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especializacion`
+--
+
+INSERT INTO `especializacion` (`id_especializacion`, `nombre_especializacion`) VALUES
+(1, 'Frontend'),
+(2, 'Backend'),
+(3, 'DevOps');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_leccion`
+--
+
+CREATE TABLE `estado_leccion` (
+  `id_estado` int(11) NOT NULL,
+  `nombre_estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estado_leccion`
+--
+
+INSERT INTO `estado_leccion` (`id_estado`, `nombre_estado`) VALUES
+(1, 'Completada'),
+(2, 'Incompleta'),
+(3, 'No iniciada');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripcion`
+--
+
+CREATE TABLE `inscripcion` (
+  `id_inscripcion` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_curso` int(11) DEFAULT NULL,
+  `inscrito_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`id_inscripcion`, `id_usuario`, `id_curso`, `inscrito_en`) VALUES
+(1, 7, 1, '2023-07-21 01:27:50'),
+(2, 55903, 1, '2023-07-21 04:22:55'),
+(4, 54731, 1, '2023-07-24 01:49:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `leccion`
+--
+
+CREATE TABLE `leccion` (
+  `id_leccion` int(11) NOT NULL,
+  `nombre_leccion` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `id_estado_leccion` int(11) DEFAULT NULL,
+  `id_curso` int(11) DEFAULT NULL,
+  `id_capitulo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `leccion`
+--
+
+INSERT INTO `leccion` (`id_leccion`, `nombre_leccion`, `descripcion`, `duracion`, `id_estado_leccion`, `id_curso`, `id_capitulo`) VALUES
+(1, 'Inputs', 'Creacion de entradas de texto y sus caracteristicas', 40, 2, 1, 1),
+(2, 'Meta', 'Diferentes tipos y utilidades', 30, 2, 1, 2),
+(3, 'Redireccionamiento', 'Enviar formularios por los distintos metodos HTTP', 40, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `progreso`
+--
+
+CREATE TABLE `progreso` (
+  `id_progreso` int(11) NOT NULL,
+  `puntuacion` int(11) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT current_timestamp(),
+  `fecha_ultimo_acceso` date DEFAULT NULL,
+  `id_inscripcion` int(11) DEFAULT NULL,
+  `id_leccion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `progreso`
+--
+
+INSERT INTO `progreso` (`id_progreso`, `puntuacion`, `fecha_inicio`, `fecha_ultimo_acceso`, `id_inscripcion`, `id_leccion`) VALUES
+(6, 0, '2023-07-23', NULL, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `email_usuario` varchar(50) NOT NULL,
+  `contraseña_usuario` varchar(100) NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `email_usuario`, `contraseña_usuario`, `creado_en`) VALUES
+(7, 'miller', 'kalednarino@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$KSEObh025XXWc2Ujw+3AaQ$qAdQ+2kmnEdlTHabUvE7t2mgttNqUOnbc+SLzU+OeRU', '2023-07-20 18:19:16'),
+(54731, 'jorgeEliasCalderon', 'jorgeEl@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$ipOeiPPbyrfJ1mN2zlAJVA$0HCunKDW8W+alQEJLRAwBFFB9ZMZBfRXQuTBO2pHdgw', '2023-07-24 01:37:13'),
+(55903, 'estebanGameplay', 'jose@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$4B4bXkBsaTYBZwleCw6brg$SI8uhTl0WCLplnnWyq46iQQlvcEOULerjMlJLMSAEVI', '2023-07-21 04:19:11');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `capitulo`
+--
+ALTER TABLE `capitulo`
+  ADD PRIMARY KEY (`id_capitulo`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`),
+  ADD UNIQUE KEY `nombre_curso` (`nombre_curso`),
+  ADD KEY `fk_docente_curso` (`id_docente`),
+  ADD KEY `fk_categoria_curso` (`id_categoria`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`id_docente`),
+  ADD KEY `fk_especializacion_docente` (`id_especializacion`);
+
+--
+-- Indices de la tabla `especializacion`
+--
+ALTER TABLE `especializacion`
+  ADD PRIMARY KEY (`id_especializacion`);
+
+--
+-- Indices de la tabla `estado_leccion`
+--
+ALTER TABLE `estado_leccion`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
+-- Indices de la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD PRIMARY KEY (`id_inscripcion`),
+  ADD KEY `fk_usuario_inscripcion` (`id_usuario`),
+  ADD KEY `fk_curso_inscripcion` (`id_curso`);
+
+--
+-- Indices de la tabla `leccion`
+--
+ALTER TABLE `leccion`
+  ADD PRIMARY KEY (`id_leccion`),
+  ADD KEY `fk_curso_leccion` (`id_curso`),
+  ADD KEY `fk_estado_leccion` (`id_estado_leccion`),
+  ADD KEY `fk_capitulo_leccion` (`id_capitulo`);
+
+--
+-- Indices de la tabla `progreso`
+--
+ALTER TABLE `progreso`
+  ADD PRIMARY KEY (`id_progreso`),
+  ADD KEY `fk_leccion_progreso` (`id_leccion`),
+  ADD KEY `fk_inscripcion_progreso` (`id_inscripcion`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email_usuario` (`email_usuario`),
+  ADD UNIQUE KEY `nombre_usuario` (`nombre_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `capitulo`
+--
+ALTER TABLE `capitulo`
+  MODIFY `id_capitulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `id_docente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `especializacion`
+--
+ALTER TABLE `especializacion`
+  MODIFY `id_especializacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_leccion`
+--
+ALTER TABLE `estado_leccion`
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  MODIFY `id_inscripcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `leccion`
+--
+ALTER TABLE `leccion`
+  MODIFY `id_leccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `progreso`
+--
+ALTER TABLE `progreso`
+  MODIFY `id_progreso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68718;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `fk_categoria_curso` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`),
+  ADD CONSTRAINT `fk_docente_curso` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id_docente`);
+
+--
+-- Filtros para la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD CONSTRAINT `fk_especializacion_docente` FOREIGN KEY (`id_especializacion`) REFERENCES `especializacion` (`id_especializacion`);
+
+--
+-- Filtros para la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD CONSTRAINT `fk_curso_inscripcion` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`),
+  ADD CONSTRAINT `fk_usuario_inscripcion` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `leccion`
+--
+ALTER TABLE `leccion`
+  ADD CONSTRAINT `fk_capitulo_leccion` FOREIGN KEY (`id_capitulo`) REFERENCES `capitulo` (`id_capitulo`),
+  ADD CONSTRAINT `fk_curso_leccion` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`),
+  ADD CONSTRAINT `fk_estado_leccion` FOREIGN KEY (`id_estado_leccion`) REFERENCES `estado_leccion` (`id_estado`);
+
+--
+-- Filtros para la tabla `progreso`
+--
+ALTER TABLE `progreso`
+  ADD CONSTRAINT `fk_inscripcion_progreso` FOREIGN KEY (`id_inscripcion`) REFERENCES `inscripcion` (`id_inscripcion`),
+  ADD CONSTRAINT `fk_leccion_progreso` FOREIGN KEY (`id_leccion`) REFERENCES `leccion` (`id_leccion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
