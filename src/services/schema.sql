36ae8a985fad27530c60848fc6678f501900a2c2
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-27 00:18:47 -0500

    refactor :recycle:  Posicion
    Se  cambiaron de posicion las insersiones del archivo.

--- src/services/schema.sql
+++ src/services/schema.sql
@@ -1,3 +1,4 @@
+-- Active: 1690155495716@@127.0.0.1@3306
 --
 -- Base de datos: `cursideslearn`
 --
@@ -17,10 +18,6 @@ CREATE TABLE `capitulo` (
 -- Volcado de datos para la tabla `capitulo`
 --
 
-INSERT INTO `capitulo` (`id_capitulo`, `nombre_capitulo`, `duracion_capitulo`) VALUES
-(1, 'Formulario', 1),
-(2, 'Metadatos', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -36,11 +33,7 @@ CREATE TABLE `categoria` (
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`) VALUES
-(1, 'Diseño de Bases de datos'),
-(3, 'UX|UI'),
-(4, 'infraestructura'),
-(5, 'Ciberseguridad');
+
 
 -- --------------------------------------------------------
 
@@ -62,8 +55,7 @@ CREATE TABLE `curso` (
 -- Volcado de datos para la tabla `curso`
 --
 
-INSERT INTO `curso` (`id_curso`, `nombre_curso`, `descripcion`, `n_horas`, `id_categoria`, `id_docente`, `creado_en`) VALUES
-(1, 'HTML', 'Aprende a crear paginas web, Estudia HTML!', 10, 3, 2, '2023-07-21 00:15:33');
+
 
 -- --------------------------------------------------------
 
@@ -81,10 +73,7 @@ CREATE TABLE `docente` (
 -- Volcado de datos para la tabla `docente`
 --
 
-INSERT INTO `docente` (`id_docente`, `nombre_docente`, `id_especializacion`) VALUES
-(1, 'Jolhver', 2),
-(2, 'Miguel', 1),
-(3, 'Bermen', 2);
+
 
 -- --------------------------------------------------------
 
@@ -101,10 +90,6 @@ CREATE TABLE `especializacion` (
 -- Volcado de datos para la tabla `especializacion`
 --
 
-INSERT INTO `especializacion` (`id_especializacion`, `nombre_especializacion`) VALUES
-(1, 'Frontend'),
-(2, 'Backend'),
-(3, 'DevOps');
 
 -- --------------------------------------------------------
 
@@ -120,11 +105,6 @@ CREATE TABLE `estado_leccion` (
 --
 -- Volcado de datos para la tabla `estado_leccion`
 --
-
-INSERT INTO `estado_leccion` (`id_estado`, `nombre_estado`) VALUES
-(1, 'Completada'),
-(2, 'Incompleta'),
-(3, 'No iniciada');
 
 -- --------------------------------------------------------
 
@@ -142,11 +122,6 @@ CREATE TABLE `inscripcion` (
 --
 -- Volcado de datos para la tabla `inscripcion`
 --
-
-INSERT INTO `inscripcion` (`id_inscripcion`, `id_usuario`, `id_curso`, `inscrito_en`) VALUES
-(1, 7, 1, '2023-07-21 01:27:50'),
-(2, 55903, 1, '2023-07-21 04:22:55'),
-(4, 54731, 1, '2023-07-24 01:49:36');
 
 -- --------------------------------------------------------
 
@@ -168,10 +143,7 @@ CREATE TABLE `leccion` (
 -- Volcado de datos para la tabla `leccion`
 --
 
-INSERT INTO `leccion` (`id_leccion`, `nombre_leccion`, `descripcion`, `duracion`, `id_estado_leccion`, `id_curso`, `id_capitulo`) VALUES
-(1, 'Inputs', 'Creacion de entradas de texto y sus caracteristicas', 40, 2, 1, 1),
-(2, 'Meta', 'Diferentes tipos y utilidades', 30, 2, 1, 2),
-(3, 'Redireccionamiento', 'Enviar formularios por los distintos metodos HTTP', 40, 1, 1, 1);
+
 
 -- --------------------------------------------------------
 
@@ -192,9 +164,6 @@ CREATE TABLE `progreso` (
 -- Volcado de datos para la tabla `progreso`
 --
 
-INSERT INTO `progreso` (`id_progreso`, `puntuacion`, `fecha_inicio`, `fecha_ultimo_acceso`, `id_inscripcion`, `id_leccion`) VALUES
-(6, 0, '2023-07-23', NULL, 4, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -212,11 +181,6 @@ CREATE TABLE `usuario` (
 --
 -- Volcado de datos para la tabla `usuario`
 --
-
-INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `email_usuario`, `contraseña_usuario`, `creado_en`) VALUES
-(7, 'miller', 'kalednarino@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$KSEObh025XXWc2Ujw+3AaQ$qAdQ+2kmnEdlTHabUvE7t2mgttNqUOnbc+SLzU+OeRU', '2023-07-20 18:19:16'),
-(54731, 'jorgeEliasCalderon', 'jorgeEl@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$ipOeiPPbyrfJ1mN2zlAJVA$0HCunKDW8W+alQEJLRAwBFFB9ZMZBfRXQuTBO2pHdgw', '2023-07-24 01:37:13'),
-(55903, 'estebanGameplay', 'jose@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$4B4bXkBsaTYBZwleCw6brg$SI8uhTl0WCLplnnWyq46iQQlvcEOULerjMlJLMSAEVI', '2023-07-21 04:19:11');
 
 --
 -- Índices para tablas volcadas
@@ -397,3 +361,60 @@ ALTER TABLE `leccion`
 ALTER TABLE `progreso`
   ADD CONSTRAINT `fk_inscripcion_progreso` FOREIGN KEY (`id_inscripcion`) REFERENCES `inscripcion` (`id_inscripcion`),
   ADD CONSTRAINT `fk_leccion_progreso` FOREIGN KEY (`id_leccion`) REFERENCES `leccion` (`id_leccion`);
+
+
+-- Insersiones de datos 
+
+INSERT INTO `capitulo` (`id_capitulo`, `nombre_capitulo`, `duracion_capitulo`) VALUES
+(1, 'Formulario', 1),
+(2, 'Metadatos', 1);
+
+
+INSERT INTO `categoria` (`id_categoria`, `nombre_categoria`) VALUES
+(1, 'Diseño de Bases de datos'),
+(3, 'UX|UI'),
+(4, 'infraestructura'),
+(5, 'Ciberseguridad');
+
+
+INSERT INTO `curso` (`id_curso`, `nombre_curso`, `descripcion`, `n_horas`, `id_categoria`, `id_docente`, `creado_en`) VALUES
+(1, 'HTML', 'Aprende a crear paginas web, Estudia HTML!', 10, 3, 2, '2023-07-21 00:15:33');
+
+
+INSERT INTO `docente` (`id_docente`, `nombre_docente`, `id_especializacion`) VALUES
+(1, 'Jolhver', 2),
+(2, 'Miguel', 1),
+(3, 'Bermen', 2);
+
+
+INSERT INTO `especializacion` (`id_especializacion`, `nombre_especializacion`) VALUES
+(1, 'Frontend'),
+(2, 'Backend'),
+(3, 'DevOps');
+
+
+INSERT INTO `estado_leccion` (`id_estado`, `nombre_estado`) VALUES
+(1, 'Completada'),
+(2, 'Incompleta'),
+(3, 'No iniciada');
+
+
+INSERT INTO `inscripcion` (`id_inscripcion`, `id_usuario`, `id_curso`, `inscrito_en`) VALUES
+(1, 7, 1, '2023-07-21 01:27:50'),
+(2, 55903, 1, '2023-07-21 04:22:55'),
+(4, 54731, 1, '2023-07-24 01:49:36');
+
+INSERT INTO `leccion` (`id_leccion`, `nombre_leccion`, `descripcion`, `duracion`, `id_estado_leccion`, `id_curso`, `id_capitulo`) VALUES
+(1, 'Inputs', 'Creacion de entradas de texto y sus caracteristicas', 40, 2, 1, 1),
+(2, 'Meta', 'Diferentes tipos y utilidades', 30, 2, 1, 2),
+(3, 'Redireccionamiento', 'Enviar formularios por los distintos metodos HTTP', 40, 1, 1, 1);
+
+
+INSERT INTO `progreso` (`id_progreso`, `puntuacion`, `fecha_inicio`, `fecha_ultimo_acceso`, `id_inscripcion`, `id_leccion`) VALUES
+(6, 0, '2023-07-23', NULL, 4, 1);
+
+
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `email_usuario`, `contraseña_usuario`, `creado_en`) VALUES
+(7, 'miller', 'kalednarino@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$KSEObh025XXWc2Ujw+3AaQ$qAdQ+2kmnEdlTHabUvE7t2mgttNqUOnbc+SLzU+OeRU', '2023-07-20 18:19:16'),
+(54731, 'jorgeEliasCalderon', 'jorgeEl@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$ipOeiPPbyrfJ1mN2zlAJVA$0HCunKDW8W+alQEJLRAwBFFB9ZMZBfRXQuTBO2pHdgw', '2023-07-24 01:37:13'),
+(55903, 'estebanGameplay', 'jose@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$4B4bXkBsaTYBZwleCw6brg$SI8uhTl0WCLplnnWyq46iQQlvcEOULerjMlJLMSAEVI', '2023-07-21 04:19:11');
